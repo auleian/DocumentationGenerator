ed2ee30b200f0aadf17b5d9b62f08531dd7592ea
auleian <aule.ian@students.mak.ac.ug>
2026-07-20 14:17:00 +0300

    Feat: This is for selecting the document type whether SRS or SAD etc

---
+++ backend/DocumentType/models.py
@@ -0,0 +1,15 @@
+#This is for selecting the type of document we want to generate whether SDD or SRS or others
+
+import uuid
+from django.db import models
+
+class DocumentType(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50, unique=True)   # e.g. "srs"
+    display_name = models.CharField(max_length=100)         # e.g. "Software Requirements Specification"
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.display_name
+
+
