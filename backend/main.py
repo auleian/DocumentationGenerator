f478c70e1679fdc0f9b94e36fe43cdd5e759636b
Namutebi Sulaina Luwambya <sulainanamutebi@gmail.com>
2026-07-24 14:24:49 +0300

    Installed packages

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,13 @@
+import sys
+from llm.llm_client import ask_llm
+
+sys.stdout.reconfigure(encoding="utf-8")
+
+
 def main():
     print("Hello from backend!")
+    response = ask_llm("Hello!")
+    print(response)
 
 
 if __name__ == "__main__":
